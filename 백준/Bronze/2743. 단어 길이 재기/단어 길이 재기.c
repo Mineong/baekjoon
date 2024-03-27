1c1250eaf84b6a2b77d6a4744f0e0225c7e5266d
MinJeong <106390284+Mineong@users.noreply.github.com>
2024-03-27 20:53:19 +0900

    Update 단어 길이 재기.c

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
+int main() {
     char word[100];
     
     scanf("%s\n", word);
-    printf("%d\n", strlen(word));
+
+    int length=0;
+    for(int i=0; word[i] !=0; i++) {
+        length+=1;
+    }
+    printf("%d\n", length);
+
     return 0;
 }
